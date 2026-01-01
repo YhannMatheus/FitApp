bec6eb7f8fd960f6fa130348eded68dc219113d9
YhannMatheus <yhannmatheus@icloud.com>
2025-12-31 21:36:59 -0300

    feat: Mudança para react native

---
+++ client/src/components/button/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button : {
+        width: '100%',
+        height: 52,
+        backgroundColor: '#161616ff',
+    }
+})
